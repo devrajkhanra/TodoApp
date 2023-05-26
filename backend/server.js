f7fa6ea0c781f31bd0c7c7b7021d1045eea85541
Devraj <khanradevraj@gmail.com>
2023-05-26 20:27:16 +0530

    Created Basic Express Server

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+// Server
+
+const express = require('express');
+const app = express();
+const PORT = 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
